39edcd6598ba502167bdfe479b8c4902dc48f1f0
ki <kisemichuk@gmail.com>
2024-08-27 08:58:59 +0400

    add install

---
+++ install.sh
@@ -0,0 +1,13 @@
+sudo dnf install google-chrome-stable zsh transmission timeshift dotnet-sdk-8.0 gnome-tweaks wireguard source-foundry-hack-fonts -y
+git clone https://github.com/semichuk/WhiteSur-gtk-theme.git
+git clone https://github.com/semichuk/WhiteSur-icon-theme.git
+git clone https://github.com/semichuk/McMojave-cursors.git
+cd McMojave-cursors/
+sudo bash install.sh
+cd ../WhiteSur-icon-theme/
+sudo bash install.sh -b
+cd ../WhiteSur-gtk-theme/
+sudo bash install.sh -o solid -P smaller
+flatpak install flathub com.github.tchx84.Flatseal
+flatpak install flathub com.raggesilver.BlackBox
+flatpak install flathub com.mattjakeman.ExtensionManager
