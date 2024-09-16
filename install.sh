ae84c63859588d35a876e8fbb6553478d510bfda
kisemichuk <70934055+semichuk@users.noreply.github.com>
2024-09-16 18:10:19 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo dnf install google-chrome-stable zsh transmission timeshift gnome-tweaks source-foundry-hack-fonts -y
+sudo dnf install google-chrome-stable zsh transmission timeshift gnome-tweaks source-foundry-hack-fonts file-roller -y
 git clone https://github.com/semichuk/WhiteSur-gtk-theme.git
 git clone https://github.com/semichuk/WhiteSur-icon-theme.git
 git clone https://github.com/semichuk/McMojave-cursors.git
