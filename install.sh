535a927bf63cca82b7ad21381f9ec731c545b73b
kisemichuk <70934055+semichuk@users.noreply.github.com>
2024-12-10 11:02:57 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 sudo dnf install google-chrome-stable fzf zsh source-foundry-hack-fonts dotnet-sdk-8.0 gnome-tweaks timeshift -y
 sudo dnf remove firefox -y
-#install vscode
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-dnf check-update
-sudo dnf install code -y
-#install docker
-sudo dnf -y install dnf-plugins-core
-sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 #install flatpak apps
 flatpak install flathub com.github.tchx84.Flatseal
 flatpak install flathub com.mattjakeman.ExtensionManager
